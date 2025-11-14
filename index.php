8aac6864cb77e30a61af8455ffdefadde8dceb80
anatolee4 <anatole.martin@sfr.fr>
2025-11-14 09:51:12 +0100

    ajout index

--- index.php
+++ index.php
@@ -1,2 +1,89 @@
-<?php
+<?php ?>
 
+<!DOCTYPE html>
+<html lang="fr" prefix="og: https://ogp.me/ns#">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="author" content="Anatole martin" />
+  <meta name="keywords" content="document html5 mmi" />
+  <meta name="description" content="Document minimal html5 en DUT MMI" />
+  <!-- Affichage standard sur périphérique mobile pour site adaptatif -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Données Open Graph -->
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="http://mmi.univ-smb.fr/~mmang/" />
+  <meta property="og:image" content="http://mmi.univ-smb.fr/~mmang/image.png" />
+  <meta property="og:locale" content="fr_FR" />
+  <meta property="og:description" content="Document minimal html5 en DUT MMI" />
+  <meta property="og:title" content="Document minimal en MMI" />
+  <meta property="og:site_name" content="Document minimal en MMI" />
+  <link rel="stylesheet" href="CV.css">
+  <title>CV du boss</title>
+</head>
+
+<body>
+    <header>
+        <h1>Anatole Martin</h1>
+    <img src="skibidi.png.png" width="300" height="590" class="skibidi">
+    
+    
+    <nav>
+    <ul>
+      <li><a href="#infos">Infos</a></li>
+      <li><a href="#formations">Formations</a></li>
+      <li><a href="#Compétences">Compétences</a></li>
+      <li><a href="#éxperiences">Expériences</a></li>
+    </ul>
+  </nav>
+
+
+    </header>
+   
+     <section id="infos">
+        <h2>info</h2>
+        <hr class="or">
+            <p><strong>Email :</strong> anatole.martin@sfr.fr | <strong>Téléphone :</strong> 06 28 66 40 30</p>
+            <p><strong>Ville :</strong> Romans-sur-isère</p>
+        </section>
+
+    <hr/>
+
+        <section id="formations">
+            <h2>formations</h2>
+            <hr class="or">
+    <table border="1">
+        <thead>
+            
+            <tr><th>dates</th><th>formations</th></tr>
+            <tr><th>2022-2023</th><th>2nd GT</th></tr>
+            <tr><th>2023-2025</th><th>1ère et terminale STI2D</th></tr>
+            <tr><th>en cours</th><th>BUT MMI</th></tr>
+
+        </thead>
+    </table>
+        </section>
+    <hr/>
+
+        <section id="Compétences">
+    <h2>compétences</h2>
+    <hr class="or">
+    <ul>
+        <li><strong>langue parlée</strong>Francais,Englais couramment</li>
+        <li><strong>Logicièle maitrisée</strong>VS code,Blender,Photoshop...</li>
+    </ul>
+</section>
+    
+    <hr/>
+
+        <section id="éxperiences">
+    <h2>expériences</h2>
+    <hr class="or">
+    <ul>
+        <li>Stage en usine</li>
+        <li>Stage chez un cuisiniste</li>
+        <li>Stage en électricité / électricien</li>
+    </ul>
+</section>
+
+</body>
